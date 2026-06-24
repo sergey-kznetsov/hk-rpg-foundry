bad2e2e56e1de73635d4fbc142eb79085bc94774
sergey-kznetsov <123492323+sergey-kznetsov@users.noreply.github.com>
2026-06-24 17:50:25 +0400

    Allow editing raw item effect arrays

--- module/sheets/item-sheets.js
+++ module/sheets/item-sheets.js
@@ -15,7 +15,22 @@ class HKBaseItemSheet extends ItemSheet {
     data.isGear = this.item.type === "gear";
     data.isShield = this.item.type === "shield";
     data.isFocus = this.item.type === "focus";
+    data.effectsJson = JSON.stringify(this.item.system?.effects ?? [], null, 2);
     return data;
+  }
+
+  async _updateObject(event, formData) {
+    const effectsJson = formData["system.effectsJson"];
+    if (effectsJson !== undefined) {
+      delete formData["system.effectsJson"];
+      try {
+        formData["system.effects"] = effectsJson?.trim() ? JSON.parse(effectsJson) : [];
+      } catch (err) {
+        ui.notifications.error("JSON эффектов предмета не распознан. Изменения эффектов не сохранены.");
+        console.error("HKRPG | item effects parse error", err);
+      }
+    }
+    return super._updateObject(event, formData);
   }
 }
 
@@ -25,7 +40,7 @@ export class HKWeaponSheet extends HKBaseItemSheet {
       classes: ["hk", "sheet", "item"],
       template: "systems/hk-rpg/templates/item-weapon-sheet.hbs",
       width: 540,
-      height: 600
+      height: 680
     });
   }
 }
@@ -35,8 +50,8 @@ export class HKArmorSheet extends HKBaseItemSheet {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["hk", "sheet", "item"],
       template: "systems/hk-rpg/templates/item-armor-sheet.hbs",
-      width: 460,
-      height: 440
+      width: 500,
+      height: 560
     });
   }
 }
@@ -46,8 +61,8 @@ export class HKGenericItemSheet extends HKBaseItemSheet {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["hk", "sheet", "item"],
       template: "systems/hk-rpg/templates/item-generic-sheet.hbs",
-      width: 560,
-      height: 560
+      width: 620,
+      height: 660
     });
   }
 }
